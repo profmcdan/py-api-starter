c65d2117d5b3491e76bd25df2039507527a505ab
Daniel Ale <danielale9291@gmail.com>
2022-03-01 15:54:33 +0100

    Design

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if ["$DATABASE" = "postgres"]
+if [ "$DEBUG" = 1 ]
 then
-    echo "Waiting for response..."
+    echo "Waiting for database response..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
         sleep 0.1
     done
 
-    echo "PostgreSQL Started!"
+    echo "Database Started!"
 
 fi
 
